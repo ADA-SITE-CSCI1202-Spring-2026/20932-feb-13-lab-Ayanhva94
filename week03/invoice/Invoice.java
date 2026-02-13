0e292b9b3b15cb44a01c391143234289e8bfba53
Ayan Hasanova <118928238+Ayanhva94@users.noreply.github.com>
2026-02-13 11:16:04 +0400

    Add InvoiceA and InvoiceB classes

--- week03/invoice/Invoice.java
+++ week03/invoice/Invoice.java
@@ -1,58 +1,50 @@
 package week03.invoice;
 
-public class Invoice {
-
+// Variant A
+public class InvoiceA {
     private String partNumber;
     private String partDescription;
     private int quantity;
     private double pricePerItem;
 
-    public Invoice(String partNumber, String partDescription, int quantity, double pricePerItem) {
+    public InvoiceA(String partNumber, String partDescription, int quantity, double pricePerItem) {
         this.partNumber = partNumber;
         this.partDescription = partDescription;
-
-        if (quantity > 0)
-            this.quantity = quantity;
-        else
-            this.quantity = 0;
-
-        if (pricePerItem > 0.0)
-            this.pricePerItem = pricePerItem;
-        else
-            this.pricePerItem = 0.0;
+        this.quantity = quantity > 0 ? quantity : 0;
+        this.pricePerItem = pricePerItem > 0.0 ? pricePerItem : 0.0;
     }
 
-    public String getPartNumber() {
-        return partNumber;
-    }
-
-    public String getPartDescription() {
-        return partDescription;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public double getPricePerItem() {
-        return pricePerItem;
-    }
-
-    public void setQuantity(int quantity) {
-        if (quantity > 0)
-            this.quantity = quantity;
-        else
-            this.quantity = 0;
-    }
-
-    public void setPricePerItem(double pricePerItem) {
-        if (pricePerItem > 0.0)
-            this.pricePerItem = pricePerItem;
-        else
-            this.pricePerItem = 0.0;
-    }
-
-    public double getInvoiceAmount() {
-        return quantity * pricePerItem;
-    }
+    public String getPartNumber() { return partNumber; }
+    public String getPartDescription() { return partDescription; }
+    public int getQuantity() { return quantity; }
+    public double getPricePerItem() { return pricePerItem; }
+    public void setQuantity(int quantity) { this.quantity = quantity > 0 ? quantity : 0; }
+    public void setPricePerItem(double pricePerItem) { this.pricePerItem = pricePerItem > 0.0 ? pricePerItem : 0.0; }
+    public double getInvoiceAmount() { return quantity * pricePerItem; }
 }
+
+// Variant B (default erişimli, eyni faylda ola bilər)
+class InvoiceB {
+    private String partNumber;
+    private String partDescription;
+    private int quantity;
+    private double pricePerItem;
+
+    public InvoiceB(String partNumber, String partDescription, int quantity, double pricePerItem) {
+        this.partNumber = partNumber;
+        this.partDescription = partDescription;
+        this.quantity = quantity > 0 ? quantity : 0;
+        this.pricePerItem = pricePerItem > 0.0 ? pricePerItem : 0.0;
+    }
+
+    public String getPartNumber() { return partNumber; }
+    public String getPartDescription() { return partDescription; }
+    public int getQuantity() { return quantity; }
+    public double getPricePerItem() { return pricePerItem; }
+    public void setPartNumber(String partNumber) { this.partNumber = partNumber; }
+    public void setPartDescription(String partDescription) { this.partDescription = partDescription; }
+    public void setQuantity(int quantity) { this.quantity = quantity > 0 ? quantity : 0; }
+    public void setPricePerItem(double pricePerItem) { this.pricePerItem = pricePerItem > 0.0 ? pricePerItem : 0.0; }
+    public double getInvoiceAmount() { return quantity * pricePerItem; }
+}
+
